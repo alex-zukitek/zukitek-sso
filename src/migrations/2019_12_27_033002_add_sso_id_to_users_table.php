0947df997d8997b255bdb46e14d73c6cb600962c
Alex <dzung.nguyen@zukitek.com>
2020-05-27 13:32:54 +0700

    add sso id to users tables

---
+++ src/migrations/2019_12_27_033002_add_sso_id_to_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddSsoUserToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (!Schema::hasColumn('users', 'sso_id')) {
+            Schema::table('users', function (Blueprint $table) {
+                $table->unsignedBigInteger('sso_id')->nullable()->after('id');
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('sso_id');
+        });
+    }
+}
